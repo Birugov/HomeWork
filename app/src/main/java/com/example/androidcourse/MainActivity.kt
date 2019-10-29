adf169238e7584011111fe7b82dd2e7e691fce3f
Birugov <asemakin403@gmail.com>
2019-10-29 12:25:11 +0400

    fixes

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.androidcourse
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             putExtra(Intent.EXTRA_TEXT, et_stext.text.toString())
             type = "text/plain"
         }
-        startActivityForResult(sendIntent, 1)
+        startActivityForResult(sendIntent, MY_CONST)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -28,5 +28,9 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1) {
             Toast.makeText(this, resultCode, Toast.LENGTH_LONG).show()
         }
+    }
+
+    companion object {
+        const val MY_CONST = 1
     }
 }
